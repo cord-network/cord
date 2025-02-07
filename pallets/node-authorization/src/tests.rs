2ad2a601e13859df5243c94d56278d22cbd73aed
zeel991 <darjizeel587@gmail.com>
2025-02-07 11:52:19 +0530

    Clippy Fix

--- pallets/node-authorization/src/tests.rs
+++ pallets/node-authorization/src/tests.rs
@@ -463,14 +463,14 @@ fn test_generate_peer_id_invalid_utf8() {
 #[test]
 fn peer_id_too_long_test() -> Result<(), Error<Test>> {
 	new_test_ext().execute_with(|| {
-        const max_peer_id_length: usize = 128;
+        const MAX_PEER_ID_LENGTH: usize = 128;
         let testing = "nodeidnodeidnodeidnodeidnodeidnodeidnodeidnodeidnodeidnodeidnodeidnodeidnodeidnodeidnodeidnodeidnodeidnodeidnodeidnodeidnodeidnodeidnodeidnodeidnodeidnodeidnodeidnodeidnodeidnodeidnodeidnodeid";
 
         let node_identity = test_node(&testing);
 		let testing = NodeAuthorization::generate_peer_id(&node_identity).unwrap();
 
-       	// assert!(testing.0.len() > max_peer_id_length,Error::<Test>::PeerIdTooLong);
-		if testing.0.len() > max_peer_id_length {
+       	// assert!(testing.0.len() > MAX_PEER_ID_LENGTH,Error::<Test>::PeerIdTooLong);
+		if testing.0.len() > MAX_PEER_ID_LENGTH {
 			return Err(Error::<Test>::PeerIdTooLong);
 		}
 		Ok(())
